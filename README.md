827f707095a6464d3f92aa20578ee874f41a44a8
Chu Shao <cshao@churenshao.com>
2014-04-01 16:53:49 -0700

    Set up boilerplate

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Boilerplate
 ===========
 
 Boilerplate for COGS 121. This should provide a basic set up for node.js. 
+Make sure you run npm install before using
---
+++ app.js
@@ -0,0 +1,28 @@
+//dependencies for each module used
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+var app = express();
+
+//route files to load
+var index = require('./routes/index');
+
+//database setup - uncomment to set up your database
+//var mongoose = require('mongoose');
+//mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
+
+//Configures the Template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+
+//routes
+app.get('/', index.view);
+//set environment ports and start application
+app.set('port', process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
